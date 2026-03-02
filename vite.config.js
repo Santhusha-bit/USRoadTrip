4b2973dcc24fa769f02dcb571271576078030dfd
santhusha <santhusha_janana@berkeley.edu>
2026-03-02 03:45:37 -0800

    Github pages setup

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // Assumes the repository name is "USRoadTrip"
+  // GitHub Pages URL will be: https://<username>.github.io/USRoadTrip/
+  base: '/USRoadTrip/',
   plugins: [react()],
   server: {
     port: 3000,
